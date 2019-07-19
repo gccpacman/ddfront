66870f20ee7d60e56e8e970a0a5c6eae962556fb
gccpacman <gccpacman@gmail.com>
2019-07-19 11:51:16 +0800

    try baidumap

--- src/components/BaiduMapDemo.vue
+++ src/components/BaiduMapDemo.vue
@@ -1,5 +1,11 @@
 <template>
-  <baidu-map class="bm-view" :center="center" :zoom="zoom" @ready="handler">
+  <baidu-map id="allmap" class="bm-view" :center="center" :zoom="zoom" @ready="handler">
+    <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+    <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+    <bm-overview-map anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :isOpen="true"></bm-overview-map>
+    <bm-marker :position="position" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
+      <bm-label :content="content" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
+    </bm-marker>
   </baidu-map>
 </template>
 
@@ -14,14 +20,18 @@ export default {
   data () {
     return {
       center: {lng: 0, lat: 0},
+      position: {lng: 0, lat: 0},
+      content: 'hello world',
       zoom: 3
     }
   },
   methods: {
     handler ({BMap, map}) {
       console.log(BMap, map)
-      this.center.lng = 116.404
-      this.center.lat = 39.915
+      this.center.lng = 121.44906
+      this.center.lat = 31.219978
+      this.position.lng = 121.44906
+      this.position.lat = 31.219978
       this.zoom = 15
     }
   }
@@ -29,8 +39,12 @@ export default {
 </script>
 
 <style scoped>
+  /* body, html,#allmap {width: 100%;height: 100%;} */
   .bm-view {
     width: 100%;
-    height: 400px;
+    height: 600px;
+    overflow: hidden;
+    margin:0;
+    font-family:"微软雅黑";
   }
 </style>
