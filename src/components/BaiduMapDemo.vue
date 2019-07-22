84f4639f0281a0345cb0e50dae1153d15187111e
gccpacman@gmail.com <gccpacman@gmail.com>
2019-07-22 16:16:23 +0800

    import gps.js

--- src/components/BaiduMapDemo.vue
+++ src/components/BaiduMapDemo.vue
@@ -1,16 +1,59 @@
 <template>
-  <baidu-map id="allmap" class="bm-view" :center="center" :zoom="zoom" @ready="handler">
-    <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
-    <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
-    <bm-overview-map anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :isOpen="true"></bm-overview-map>
-    <bm-marker :position="position" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
-      <bm-label :content="content" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
-    </bm-marker>
-  </baidu-map>
+  <div class='container'>
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <p class="subtitle is-5">
+              <strong>123</strong> posts
+            </p>
+          </div>
+          <div class="level-item">
+            <div class="field has-addons">
+              <p class="control">
+                <input class="input" type="text" placeholder="Find a post">
+              </p>
+              <p class="control">
+                <button class="button">
+                  Search
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="level-right">
+          <p class="level-item"><strong>All</strong></p>
+          <p class="level-item"><a>Published</a></p>
+          <p class="level-item"><a>Drafts</a></p>
+          <p class="level-item"><a>Deleted</a></p>
+          <p class="level-item"><a class="button is-success">New</a></p>
+        </div>
+      </div>
+      <div class="media">
+        <div class="media-content">
+          <div class="content">
+            <baidu-map id="allmap" class="bm-view" center="上海市" :zoom="zoom" @ready="handler">
+              <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+              <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+              <bm-overview-map anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :isOpen="true"></bm-overview-map>
+              <bm-marker :position="position" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
+                <bm-label :content="content" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
+              </bm-marker>
+              <bm-marker :position="position2" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
+                <bm-label :content="content2" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
+              </bm-marker>
+            </baidu-map>
+          </div>
+        </div>
+        <div class="level-right">
+          <p class="content"><strong>Hello world</strong></p>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
 // import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+import { GPS } from '../gps.js'
 
 export default {
   name: 'BaiduMapDemo',
@@ -19,20 +62,18 @@ export default {
   // },
   data () {
     return {
-      center: {lng: 0, lat: 0},
-      position: {lng: 0, lat: 0},
-      content: 'hello world',
-      zoom: 3
+      center: {lng: 121.44906, lat: 31.219978},
+      position: {lng: 121.44906, lat: 31.219978},
+      content: '马路一',
+      position2: {lng: 121.44106, lat: 31.216978},
+      content2: '建筑二',
+      zoom: 14
     }
   },
   methods: {
     handler ({BMap, map}) {
       console.log(BMap, map)
-      this.center.lng = 121.44906
-      this.center.lat = 31.219978
-      this.position.lng = 121.44906
-      this.position.lat = 31.219978
-      this.zoom = 15
+      console.log(GPS.PI)
     }
   }
 }
@@ -45,6 +86,7 @@ export default {
     height: 600px;
     overflow: hidden;
     margin:0;
+    padding: 5 5 5 5;
     font-family:"微软雅黑";
   }
 </style>
