a4b386656c0ccc28a2af10f36851e183513e4b12
gccpacman@gmail.com <gccpacman@gmail.com>
2019-08-05 10:20:54 +0800

    update road ui

--- src/components/Road.vue
+++ src/components/Road.vue
@@ -1,33 +1,35 @@
 <template>
   <section>
-    <div>
-      <p>Road {{ $route.params.id }}</p>
-      <section class="hero">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">
-              {{ title }}
-            </h1>
-            <h2 class="subtitle">
-              {{ des2 }}
-            </h2>
-          </div>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            {{ title }}
+          </h1>
+          <h2 class="subtitle">
+            {{ place_name }}
+          </h2>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
     <div class="bmap-container is-gapless">
       <baidu-map id="allmap" class="bm-view" :center="center" :zoom="zoom" @ready="handler">
         <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
         <template v-for="(polylinePath, index) in polylinePathList">
-          <bm-polyline v-bind:item="polylinePath" v-bind:key="index" :path="polylinePath" stroke-color="blue" :stroke-opacity="1" :stroke-weight="5" :editing="false"></bm-polyline>
+          <bm-polyline v-bind:item="polylinePath" v-bind:key="index" :path="polylinePath" stroke-color="blue" :stroke-opacity="1" :stroke-weight="2" :editing="false"></bm-polyline>
         </template>
         <template v-for="arch in archList">
           <bm-marker v-bind:item="arch" v-bind:key="arch.id" :position="arch.position" :dragging="false">
-            <bm-label :content="arch.name" :labelStyle="{color: 'black', fontSize : '10px', backgroundColor :'rgba(255,255,255,0.5)', border: '0px'}" :offset="{width: -20, height: 15}"/>
+            <bm-label :content="arch.name" :labelStyle="{color: 'black', fontSize : '10px', backgroundColor :'rgba(255,255,255,0.3)', border: '1px'}" :offset="{width: -20, height: 10}"/>
           </bm-marker>
         </template>
       </baidu-map>
     </div>
+    <article class="message is-small is-dark">
+      <div class="message-body">
+        <p>{{ des }}</p>
+      </div>
+    </article>
   </section>
 </template>
 
@@ -38,14 +40,11 @@ export default {
     handler ({BMap, map}) {
       console.log(BMap, map)
       this.axios.get(process.env.ROOT_API + '/road/' + this.$route.params.id).then((response) => {
+        this.title = response.data.name_chs
+        this.des = response.data.des2
+        this.place_name = response.data.place_name
         this.polylinePathList = response.data.polylines_bmap
         this.center = response.data.center_bmap
-        this.title = response.data.name_chs
-        this.des2 = response.data.des2
-        console.log(response)
-        console.log(this.polylinePathList)
-        console.log(this.polylinePathList[0])
-        console.log(this.center)
         var archItems = response.data.road_architecture
         if (archItems && archItems.length > 0) {
           for (var i = 0; i < archItems.length; i++) {
@@ -59,6 +58,8 @@ export default {
             })
           }
         }
+      }).catch(function (error) {
+        console.log(error)
       })
     }
   },
@@ -68,11 +69,12 @@ export default {
   data () {
     return {
       center: null,
-      zoom: 14,
+      zoom: 15,
       polylinePathList: [],
       archList: [],
       title: '路名',
-      des2: '描述'
+      des: '描述',
+      place_name: '所在区'
     }
   }
 }
@@ -85,7 +87,7 @@ export default {
   }
   .bm-view {
     width: 100%;
-    height: 400px;
+    height: 330px;
     overflow: hidden;
     margin:0;
     padding: 0 0 0 0;
