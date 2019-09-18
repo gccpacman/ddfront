2b24e3ee5b1688c4b8a7a8ccceba3a7f136072a6
gccpacman@gmail.com <gccpacman@gmail.com>
2019-09-18 18:25:03 +0800

    add google analysis

--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Axios from 'axios'
+import VueAnalytics from 'vue-analytics'
 
 Vue.prototype.$axios = Axios
 Vue.config.productionTip = false
+
+if (process.env.GA) {
+  Vue.use(VueAnalytics, {
+    id: process.env.GA,
+    disableScriptLoader: true,
+    router,
+    autoTracking: {
+      pageviewOnLoad: false
+    }
+  })
+}
 
 /* eslint-disable no-new */
 new Vue({
