3985142bb451dc0156e147aabbb655ec6fadab33
gccpacman <gccpacman@gmail.com>
2019-10-27 14:55:32 +0800

    change api url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://api.datadude.xyz/api"',
+  ROOT_API: '"https://api2.datadude.xyz/api"',
   GA: '"UA-146619367-1"'
 }
