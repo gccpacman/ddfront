dedb1ee938f0a751197c478155795396735f18fe
gccpacman <gccpacman@gmail.com>
2019-11-10 01:55:54 +0800

    update build static prefix

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // ROOT_API: '"http://127.0.0.1:8000/api"'
-  ROOT_API: '"http://api.datadude.xyz/api"'
+  ROOT_API: '"http://127.0.0.1:8000/api"'
+  // ROOT_API: '"http://api.datadude.xyz/api"'
 })
--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://staticfiles.datadude.xyz/.dev/',
 
     /**
      * Source Maps
